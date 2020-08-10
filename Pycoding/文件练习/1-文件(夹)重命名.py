3f9c22a6b76bc3c4a992bde071f0932bbc9c1136
Orzsxy <921560202@qq.com>
2020-08-10 22:47:30 +0800

    2020.8.10

--- Pycoding/文件练习/1-文件(夹)重命名.py
+++ Pycoding/文件练习/1-文件(夹)重命名.py
@@ -1,9 +1,9 @@
 import os;
 import re;
 def rename():
-	path = 'C:\\Users\\DELL\\Desktop\\wp_icon\\P';
+	path = 'C:\\Users\\DELL\\Desktop\\wp_icon';
 	filelist = os.listdir(path);
-	i = 1
+	i = 32
 	for file in filelist:
 		oldir = os.path.join(path,file);#join用来连接路径
 		if os.path.isdir(oldir):
@@ -28,55 +28,4 @@ os.path.splitext(path)会返回    (D:/User/wgy/workplace/data/notMNIST_large.ta
 '''
 '''rename(src,drc) 分别是原来的路径(文件名)、换之后的路径(文件名)
 如果drc未改之前已经存在了，会返回错误(不能出现两个重名的)
-'''
-def rename1():
-	path = 'C:\\Users\\DELL\\Desktop';
-	mark = addinfo
-	filelist = os.listdir(path); #不会递归调用，只显示path根目录下的内容
-	for file in filelist:
-		if file.endswith('.txt'):	#看是不是以txt结尾的文件。 	#if file != sys.argv[0]:可以用来判断此时读取的文件是不是当前的程序
-			os.rename(file,mark+file)
-fileList = []
-def getfile_listdir(dirPath):
-	# Function can get *.jpg 
-    files = os.listdir(dirPath)
-    # re match *.xls/xlsx，you can change 'xlsx' to 'doc' or other file types.
-    ptn = re.compile('.*\.jpg')
-    for f in files:
-        # isdir, call self
-        if (os.path.isdir(dirPath + '\\' + f)):
-            getfile_listdir(dirPath + '\\' + f)
-        # isfile, judge
-        elif (os.path.isfile(dirPath + '\\' + f)):
-            res = ptn.match(f)
-            if (res != None):
-                fileList.append(dirPath + '\\' + res.group())
-        else:
-            fileList.append(dirPath + '\\无效文件')
-if __name__ == '__main__':
-	pa = 'C:\\Users\\DELL\\Desktop\\wp_icon'
-	getfile_listdir(pa)
-	print("提取结果：")
-	for f in fileList:
-		print(f)
-
-
-'''
-def getfiles_walk(dirPath):
-	#home,dirs,
-	files = os.listdir(dirPath)
-    ptn=re.compile('.*\.jpg')
-    for f in files:
-        res = ptn.match(f)
-        if (res != None):
-            fileList.append(dirPath + '\\' + res.group())
-        else:
-            fileList.append(dirPath + '\\无效文件')
-if __name__ == '__main__':
-	pa = 'C:\\Users\\DELL\\Desktop\\wp_icon'
-	getfiles_walk(pa)
-	print("提取结果：")
-	for f in fileList:
-		print(f)
-#help(re.compile)
 '''
